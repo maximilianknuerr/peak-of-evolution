204f620e6ed0176a2cb952b1b78a1e46712486da
Maximilian Knürr <max@MacBook-Pro-von-Maximilian.local>
2022-03-15 09:48:30 +0100

    vuetify installed + navbar implemented

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-main>
+      <NavBar/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from '@/components/Navbar.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    NavBar,
+  },
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@font-face {
+  font-family: "DMSans";
+  src: url("fonts/DMSans/DMSans-Regular.ttf");
 }
+
+@font-face {
+  font-family: "Migra";
+  src: url("fonts/Migra_Headline/MigraItalic-ExtraboldItalic.otf");
+}
+
+.v-application{
+     font-family: "DMSans";
+     font-size: 20px;
+     background-color: #EBE5D2;
+  }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-toolbar height=100 color="rgba(235, 229, 210, 1)" class="navbar" dense>
+
+      <v-toolbar-side-icon>
+            <v-img class="mr-3" src="@/assets/LOGO.png" height="80px" width="80px"> 
+            </v-img>
+     </v-toolbar-side-icon>
+
+      <v-spacer></v-spacer>
+
+      <v-btn class="navbar-btn">
+        ABOUT
+      </v-btn>
+
+      <v-btn class="navbar-btn">
+        FACTS
+      </v-btn>
+
+      <v-btn class="navbar-btn">
+        TEAM
+      </v-btn>
+
+      <v-btn class="navbar-btn">
+        TRAILER
+      </v-btn>
+      <v-btn class="navbar-btn">
+        CONTACT
+      </v-btn>
+    </v-toolbar>
+</template>
+
+<script>
+
+export default {
+  name: 'NavBar',
+
+  data: () => ({
+    
+  }),
+}
+</script>
+<style scoped>
+  .navbar-btn{
+    font-size: 30px;
+  }
+</style>
