9057bf64f5240733533ad85738e069819f53c4a2
Maximilian Knürr <max@MacBook-Pro-von-Maximilian.local>
2022-03-15 17:23:38 +0100

    responsive design

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-toolbar height=100 color="rgba(235, 229, 210, 1)" class="navbar" dense>
+  <div>
+    <v-app-bar height=100 color="rgba(235, 229, 210, 1)" class="navbar" fixed app elevation=0>
 
       <v-toolbar-side-icon>
             <v-img class="mr-3" src="@/assets/LOGO.png" height="80px" width="80px"> 
@@ -8,25 +9,33 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn to="/about" class="navbar-btn">
-        ABOUT
-      </v-btn>
+      <div v-if="!isMobile">
+        <v-btn to="/about" class="navbar-btn">
+          ABOUT
+        </v-btn>
 
-      <v-btn class="navbar-btn">
-        FACTS
-      </v-btn>
+        <v-btn class="navbar-btn">
+          FACTS
+        </v-btn>
 
-      <v-btn class="navbar-btn">
-        TEAM
-      </v-btn>
+        <v-btn class="navbar-btn">
+          TEAM
+        </v-btn>
 
-      <v-btn class="navbar-btn">
-        TRAILER
+        <v-btn class="navbar-btn">
+          TRAILER
+        </v-btn>
+        <v-btn to="/contact" class="navbar-btn">
+          CONTACT
+        </v-btn>
+      </div>
+
+      <v-btn v-if="isMobile">
+        <v-img src="@/assets/Menu.png"></v-img>
       </v-btn>
-      <v-btn to="/contact" class="navbar-btn">
-        CONTACT
-      </v-btn>
-    </v-toolbar>
+    </v-app-bar>
+    
+  </div>
 </template>
 
 <script>
@@ -37,6 +46,11 @@ export default {
   data: () => ({
     
   }),
+  computed: {
+  isMobile() {
+    return this.$vuetify.display.smAndDown;
+  }
+  }
 }
 </script>
 <style scoped>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -13,7 +13,6 @@
             Evolving from fish and having become comical apes, we give ourselves and small everyday situations around us an absurd amount of importance. Dressed in clothes, sitting at an office chair, we try to give ourselves meaning and create a world for ourselves in which we have importance. We take everything far too seriously, from everyday embarrassments to invented systems into which we willingly place ourselves.
           </p>
           <br>
-          <br>
           <p>
             <span style="font-family: Migra; color: #FF5808">Peak of Evolution</span>
              explores the systems, rituals and customs we have created to follow a preconceived path that gives us meaning and sets us apart from „mere animals“. As humanity, we have glorified ourselves and taken human narcissism to the extreme.
@@ -42,20 +41,19 @@ export default {
   bottom: 0;
   left: 0;
   margin-top: 110px;
-  margin-left: 200px;
+  margin-left: 15%;
   z-index: 1;
-  
-
 }
 
 .about-text {
   position: absolute;
+  font-size: 35px;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
-  margin-top: 200px;
-  margin-left: 375px;
+  margin-top: 170px;
+  margin-left: calc(15% + 175px);
   margin-right: 50px;
   z-index: 5;
 }
