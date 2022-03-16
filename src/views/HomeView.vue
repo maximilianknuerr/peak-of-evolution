8144faea586e5d31fe31f4a95deb97ff8b60b0bc
Maximilian Knürr <max@MacBook-Pro-von-Maximilian.local>
2022-03-16 14:31:20 +0100

    added HomeView for mobile

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+    <list-menu v-if="isMobile"></list-menu>
+</template>
+<script>
+import ListMenu from '@/components/ListMenu.vue'
+export default {
+  name: 'HomeView',
+  components: {
+    ListMenu,
+  },
+  data: () => ({
+    //
+  }),
+  computed: {
+  isMobile() {
+    return this.$vuetify.display.smAndDown;
+  }
+  }
+}
+</script>
