7d004954aa7ff60a9b77c298f2accc44a31078c1
Maximilian Knürr <max@MacBook-Pro-von-Maximilian.local>
2022-03-16 22:00:56 +0100

    teamView implemented

---
+++ src/views/TeamView.vue
@@ -0,0 +1,18 @@
+<template>
+    <construction-view></construction-view>
+</template>
+<script>
+import ConstructionView from './ConstructionView.vue'
+export default {
+  name: 'TeamView',
+    components: {
+        ConstructionView,
+    },
+  data: () => ({
+    //
+  }),
+  
+}
+</script>
+<style scoped>
+</style>
