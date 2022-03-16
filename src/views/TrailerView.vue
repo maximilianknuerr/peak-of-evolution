d14bed1c7a68110f123a91586339e056ff677ee1
Maximilian Knürr <max@MacBook-Pro-von-Maximilian.local>
2022-03-16 22:01:10 +0100

    trailerview implemented

---
+++ src/views/TrailerView.vue
@@ -0,0 +1,18 @@
+<template>
+    <construction-view></construction-view>
+</template>
+<script>
+import ConstructionView from './ConstructionView.vue'
+export default {
+  name: 'TrailerView',
+    components: {
+        ConstructionView,
+    },
+  data: () => ({
+    //
+  }),
+  
+}
+</script>
+<style scoped>
+</style>
