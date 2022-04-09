48b88fd161a25c15212e6b564e7e79d365ba3d41
Maximilian Knürr <max@MacBook-Pro-von-Maximilian.local>
2022-04-09 14:05:38 +0200

    About view scrolls and is responsive

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,23 +1,27 @@
 <template>
     <div>
-        <v-container>
+        <v-container class="mr-0 ml-0" style="max-width: 100% !important">
           <v-row>
-          <v-img src="@/assets/Stern.png" class="bgimg rotating"></v-img>
-          <div class="about-text">
-            <br>
-          <br>
-          <p>
-            The emergence of man was mere coincidence.
-          </p>
-          <p>
-            Evolving from fish and having become comical apes, we give ourselves and small everyday situations around us an absurd amount of importance. Dressed in clothes, sitting at an office chair, we try to give ourselves meaning and create a world for ourselves in which we have importance. We take everything far too seriously, from everyday embarrassments to invented systems into which we willingly place ourselves.
-          </p>
-          <br>
-          <p>
-            <span style="font-family: Migra; color: #FF5808">Peak of Evolution</span>
-             explores the systems, rituals and customs we have created to follow a preconceived path that gives us meaning and sets us apart from „mere animals“. As humanity, we have glorified ourselves and taken human narcissism to the extreme.
-          </p>
-          </div>
+            <v-col cols=1></v-col>
+            <v-col cols=11 class="pl-0">
+              <v-img src="@/assets/Stern.png" class="bgimg rotating"></v-img>
+              <div class="about-text">
+                <br>
+              <br>
+              <p>
+                The emergence of man was mere coincidence.
+              </p>
+              <p>
+                Evolving from fish and having become comical apes, we give ourselves and small everyday situations around us an absurd amount of importance. Dressed in clothes, sitting at an office chair, we try to give ourselves meaning and create a world for ourselves in which we have importance. We take everything far too seriously, from everyday embarrassments to invented systems into which we willingly place ourselves.
+              </p>
+              <br>
+              <p>
+                <span style="font-family: Migra; color: #FF5808">Peak of Evolution</span>
+                explores the systems, rituals and customs we have created to follow a preconceived path that gives us meaning and sets us apart from „mere animals“. As humanity, we have glorified ourselves and taken human narcissism to the extreme.
+              </p>
+              </div>
+            </v-col>
+          
           </v-row>
         </v-container>
     </div>
@@ -46,14 +50,14 @@ export default {
 }
 
 .about-text {
-  position: absolute;
+  position: relative;
   font-size: 35px;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
   margin-top: 165px;
-  margin-left: calc(15% + 175px);
+  margin-left: calc(7% + 175px);
   margin-right: 50px;
   z-index: 5;
 }
